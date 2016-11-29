7b4df5d76652c1733a12595989d8d0ad8f0ac2a8
fanyuanbo <fanyuanbo@qileitekiMacBook-Air.local>
2016-11-29 13:57:51 +0800

    second project

---
+++ fanfan66/fanfan66/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  fanfan66
+//
+//  Created by fanyuanbo on 2016/11/29.
+//  Copyright © 2016年 fanyuanbo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ fanfan66/fanfan66/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  fanfan66
+//
+//  Created by fanyuanbo on 2016/11/29.
+//  Copyright © 2016年 fanyuanbo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
